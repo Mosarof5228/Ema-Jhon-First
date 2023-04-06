55a06ec3228d65b23b6d01ef70b926d27a13c44d
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-04-07 00:40:20 +0600

    router defiine and route set with component

--- src/assets/components/CART/Cart.jsx
+++ src/assets/components/CART/Cart.jsx
@@ -10,6 +10,7 @@ const Cart = (props) => {
     let totalShipping = 0;
     let quantity = 0;
     for (const product of cart) {
+        //product.quantity jodi true hoy mane product.quantity=0 na hoy tahole product.quantity naw or product.quantity=1; dia daw.(0 is a falsy value.)
         product.quantity = product.quantity || 1;
         totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,45 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './assets/components/MainLayout/Home';
+import Shop from './assets/components/SHOP/Shop';
+import Orders from './assets/components/Orders/Orders';
+import Inventory from './assets/components/Inventory/Inventory';
+import Login from './assets/components/Login/Login'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>,
+      },
+      {
+        path: '/Orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'Inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
